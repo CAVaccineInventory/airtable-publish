b4222f42f616ab2e6069ef4f0a48f463289cc6b8
Alex Vandiver <alex@chmrr.net>
2021-01-28 15:26:58 -0800

    testing: Improve local testing instructions.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,21 @@
 
 # This script is only intended for environment setup.
 
-AIRTABLE_KEY_TEMP=$(gcloud secrets versions access 1 --secret="airtable-key")
-if [ ! -z "$AIRTABLE_KEY_TEMP" ]; then
+AIRTABLE_KEY_TEMP=$(gcloud secrets versions access 1 --secret="airtable-key" 2>/dev/null)
+if [ -n "$AIRTABLE_KEY_TEMP" ]; then
 	export AIRTABLE_KEY=${AIRTABLE_KEY_TEMP}
+fi
+
+if [ -f /testing-key.json ]; then
+	export GOOGLE_APPLICATION_CREDENTIALS=/testing-key.json
+	gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+elif [ -d /testing-key.json ]; then
+	echo
+	echo "Testing file specified, but not found on your host."
+	echo
+	echo "Check the path to your testing key that you passed as"
+	echo "the first part to -v; it has been created as a directory."
+	exit 1
 fi
 
 echo "Starting exporter service..."
