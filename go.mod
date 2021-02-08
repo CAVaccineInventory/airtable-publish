32b1932f84676f6470b05c0d3ed205155295eb89
Robert <rspier@pobox.com>
2021-02-07 20:30:36 -0800

    Get ProjectID from the current credentials instead of metadata server.
    This allows it to transparently work in Cloud/in our containers/locally.

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/stretchr/testify v1.7.0
 	go.opencensus.io v0.22.5
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	google.golang.org/genproto v0.0.0-20200806141610-86f49bd18e98
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 )
--- pipeline/pkg/config/stackdriver.go
+++ pipeline/pkg/config/stackdriver.go
@@ -1,13 +1,18 @@
 package config
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"cloud.google.com/go/compute/metadata"
+	traceapi "cloud.google.com/go/trace/apiv2"
 	"contrib.go.opencensus.io/exporter/stackdriver"
+	"golang.org/x/oauth2/google"
 	"google.golang.org/genproto/googleapis/api/monitoredres"
 )
 
@@ -15,6 +20,39 @@ var (
 	// This can be overridden in tests to set timeouts.
 	httpClient *http.Client = nil
 )
+
+func getProject() (string, error) {
+
+	// There's no obvious context to use here, so create one.  It would be
+	// possible to plumb one through, but there's no obvious context available
+	// to plumb.
+	ctx, cxl := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cxl()
+
+	// Attempt to get project from local credentials.  This will work both on
+	// Cloud or in a local machine.  On Cloud, we could use the metadata server,
+	// but it's simpler to only implement one way.
+	creds, err := google.FindDefaultCredentials(ctx, traceapi.DefaultAuthScopes()...)
+	if err != nil {
+		return "", fmt.Errorf("FindDefaultCredentials: %v", err)
+	}
+	if creds.ProjectID == "" {
+		return "", errors.New("FindDefaultCredentials: no project found with application default credentials")
+	}
+	return creds.ProjectID, nil
+}
+
+func getInstanceID(mc *metadata.Client) (string, error) {
+	inst, err := mc.InstanceID()
+	if err != nil {
+		return "", fmt.Errorf("metadata.InstanceID(): %w", err)
+	}
+	// InstanceIDs are really long, just pick the right-most characters.
+	if len(inst) > 8 {
+		inst = inst[len(inst)-8:]
+	}
+	return inst, nil
+}
 
 // Taken from https://github.com/census-ecosystem/opencensus-go-exporter-stackdriver/blob/master/stats.go#L182
 func getTaskID() string {
@@ -29,7 +67,7 @@ func getTaskID() string {
 func StackdriverOptions(namespace string) stackdriver.Options {
 	mc := metadata.NewClient(httpClient)
 
-	id, err := mc.InstanceID()
+	id, err := getInstanceID(mc)
 	if err != nil {
 		id = getTaskID()
 	}
@@ -39,12 +77,12 @@ func StackdriverOptions(namespace string) stackdriver.Options {
 		location = "unknown"
 	}
 
-	prj, err := mc.ProjectID()
+	prj, err := getProject()
 	if err != nil {
 		prj = "unknown"
 	}
 
-	return stackdriver.Options{
+	so := stackdriver.Options{
 		ProjectID:         prj,
 		ReportingInterval: 60 * time.Second,
 		Resource: &monitoredres.MonitoredResource{
@@ -57,4 +95,6 @@ func StackdriverOptions(namespace string) stackdriver.Options {
 			},
 		},
 	}
+	log.Printf("StackDriver Labels: %+v", so.Resource.Labels) //nolint
+	return so
 }
