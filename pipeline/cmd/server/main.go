CAVaccineInventory/airtable-publish

commit 7f1752dd8b463e26db561f59014b525a131fcef4

deploys: Make writing to local direcotry default for testing deploys.
This switches the `-local` flag to effectively be the default for
testing deploys, by storing the storage function on the deploy
information.  Testing uploads is done by passing the `-bucket` to
store in, and skipping the local output is accomplished by passing
`-noop`.

pipeline/cmd/server/main.go
@@ -16,6 +16,7 @@ import (
 	"github.com/CAVaccineInventory/airtable-export/pipeline/pkg/generator"
 	"github.com/CAVaccineInventory/airtable-export/pipeline/pkg/metrics"
 	"github.com/CAVaccineInventory/airtable-export/pipeline/pkg/secrets"
+	"github.com/CAVaccineInventory/airtable-export/pipeline/pkg/storage"
 	beeline "github.com/honeycombio/beeline-go"
 	"github.com/honeycombio/beeline-go/trace"
 	"github.com/honeycombio/beeline-go/wrappers/hnynethttp"
@@ -25,18 +26,17 @@ import (
 type Publisher struct {
 	lastPublishSucceeded bool // Make this thread safe if nontrivial multithreading comes up.
 	deploy               deploys.DeployType
-	publishManager       func() *generator.PublishManager
 }
 
 // Takes the Google Cloud Storage bucket path as the first argument.
 func main() {
 	noopFlag := flag.Bool("noop", false, "Only print output, don't upload")
-	localFlag := flag.Bool("local", false, "Write to files under local/")
-	metricsFlag := flag.Bool("metrics", true, "Enable metrics reporting")
+	bucketFlag := flag.String("bucket", "", "Upload into a specific bucket")
+	metricsFlag := flag.Bool("metrics", false, "Enable metrics reporting")
 	flag.Parse()
 
-	if *localFlag && *noopFlag {
-		log.Fatal("-noop and -local are mutually exclusive!")
+	if *noopFlag && *bucketFlag != "" {
+		log.Fatal("-noop and -bucket are mutually exclusive!")
 	}
 
 	secrets.RequireAirtableSecret()
@@ -55,14 +55,14 @@ func main() {
 		defer metricsCleanup()
 	}
 
-	p := Publisher{}
 	if *noopFlag {
-		p.publishManager = generator.NewNoopPublishManager
-	} else if *localFlag {
-		p.publishManager = generator.NewLocalPublishManager
-	} else {
-		p.publishManager = generator.NewPublishManager
+		// "bucket-name" is arbitrary here, since nothing is written anywhere
+		deploys.SetTestingStorage(storage.DebugToSTDERR, "bucket-name")
+	} else if *bucketFlag != "" {
+		deploys.SetTestingStorage(storage.UploadToGCS, *bucketFlag)
 	}
+
+	p := Publisher{}
 	log.Println("Starting...")
 	p.Run()
 }
@@ -107,7 +107,7 @@ func (p *Publisher) syncAndPublishRequest(w http.ResponseWriter, r *http.Request
 	ctx, cxl := context.WithTimeout(ctx, time.Duration(timeoutMinutes)*time.Minute)
 	defer cxl()
 
-	pm := p.publishManager()
+	pm := generator.NewPublishManager()
 	p.lastPublishSucceeded = pm.PublishAll(ctx)
 	if !p.lastPublishSucceeded {
 		w.WriteHeader(http.StatusInternalServerError)

pipeline/cmd/once/main.go
@@ -6,20 +6,22 @@ import (
 	"log"
 	"os"
 
+	"github.com/CAVaccineInventory/airtable-export/pipeline/pkg/deploys"
 	"github.com/CAVaccineInventory/airtable-export/pipeline/pkg/generator"
 	"github.com/CAVaccineInventory/airtable-export/pipeline/pkg/metrics"
 	"github.com/CAVaccineInventory/airtable-export/pipeline/pkg/secrets"
+	"github.com/CAVaccineInventory/airtable-export/pipeline/pkg/storage"
 )
 
 // Takes the Google Cloud Storage bucket path as the first argument.
 func main() {
 	noopFlag := flag.Bool("noop", false, "Only print output, don't upload")
-	localFlag := flag.Bool("local", false, "Write to files under local/")
+	bucketFlag := flag.String("bucket", "", "Upload into a specific bucket")
 	metricsFlag := flag.Bool("metrics", false, "Enable metrics reporting")
 	flag.Parse()
 
-	if *localFlag && *noopFlag {
-		log.Fatal("-noop and -local are mutually exclusive!")
+	if *noopFlag && *bucketFlag != "" {
+		log.Fatal("-noop and -bucket are mutually exclusive!")
 	}
 
 	secrets.RequireAirtableSecret()
@@ -29,14 +31,15 @@ func main() {
 		defer metricsCleanup()
 	}
 
-	var pm *generator.PublishManager
+	pm := generator.NewPublishManager()
+
 	if *noopFlag {
-		pm = generator.NewNoopPublishManager()
-	} else if *localFlag {
-		pm = generator.NewLocalPublishManager()
-	} else {
-		pm = generator.NewPublishManager()
+		// "bucket-name" is arbitrary here, since nothing is written anywhere
+		deploys.SetTestingStorage(storage.DebugToSTDERR, "bucket-name")
+	} else if *bucketFlag != "" {
+		deploys.SetTestingStorage(storage.UploadToGCS, *bucketFlag)
 	}
+
 	ok := pm.PublishAll(context.Background())
 	if !ok {
 		os.Exit(1)

pipeline/pkg/deploys/deploy_config.go
@@ -1,6 +1,14 @@
 package deploys
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/CAVaccineInventory/airtable-export/pipeline/pkg/endpoints/metadata"
+)
+
+// A function which can be used to output the transformed data; see pkg/storage/
+type StorageWriter func(ctx context.Context, destinationFile string, transformedData metadata.JSONData) error
 
 type Bucket struct {
 	Name     string
@@ -33,6 +41,7 @@ func (b *Bucket) GetDownloadURL() string {
 
 // Pair of legacy bucket, and new bucket hooked up to a CDN
 type DeployConfig struct {
+	Storage      StorageWriter
 	LegacyBucket Bucket
 	APIBucket    Bucket
 }

pipeline/pkg/deploys/deploys.go
@@ -1,9 +1,10 @@
 package deploys
 
 import (
-	"errors"
 	"fmt"
 	"os"
+
+	"github.com/CAVaccineInventory/airtable-export/pipeline/pkg/storage"
 )
 
 // Versions are, practically, numbers, but we use strings for
@@ -26,18 +27,22 @@ const (
 // the same bucket but separate directories; in the non-legacy
 // version, they're in the top level of separate buckets, at separate
 // domains names.
-var deploys = map[DeployType]DeployConfig{
+var deploys = map[DeployType]*DeployConfig{
 	DeployTesting: {
+		// The bucket name here used for the name of the local
+		// directory to write into.
+		Storage: storage.StoreLocal,
 		LegacyBucket: Bucket{
-			// name is set below
+			Name: "local",
 			Path: "legacy",
 		},
 		APIBucket: Bucket{
-			// name is set below
+			Name: "local",
 			Path: "api",
 		},
 	},
 	DeployStaging: {
+		Storage: storage.UploadToGCS,
 		LegacyBucket: Bucket{
 			Name: "cavaccineinventory-sitedata",
 			Path: "airtable-sync-staging",
@@ -48,6 +53,7 @@ var deploys = map[DeployType]DeployConfig{
 		},
 	},
 	DeployProduction: {
+		Storage: storage.UploadToGCS,
 		LegacyBucket: Bucket{
 			Name: "cavaccineinventory-sitedata",
 			Path: "airtable-sync",
@@ -71,25 +77,28 @@ func GetDeploy() (DeployType, error) {
 	return deploy, nil
 }
 
-// Fills out the bucket information from the TESTING_BUCKET
-// environment variable if in testing.
+// Returns the deployment configuration.
 func getDeployConfig() (*DeployConfig, error) {
 	deploy, err := GetDeploy()
 	if err != nil {
 		return nil, err
 	}
 	config := deploys[deploy]
-	if deploy != DeployTesting {
-		return &config, nil
-	}
+	return config, nil
+}
 
-	bucketName := os.Getenv("TESTING_BUCKET")
-	if bucketName == "" {
-		return nil, errors.New("Set TESTING_BUCKET env var to the name of your bucket (see README.md)")
+func GetStorage() (StorageWriter, error) {
+	config, err := getDeployConfig()
+	if err != nil {
+		return nil, err
 	}
-	config.LegacyBucket.Name = bucketName
-	config.APIBucket.Name = bucketName
-	return &config, nil
+	return config.Storage, nil
+}
+
+func SetTestingStorage(sw StorageWriter, bucketName string) {
+	deploys[DeployTesting].Storage = sw
+	deploys[DeployTesting].LegacyBucket.Name = bucketName
+	deploys[DeployTesting].APIBucket.Name = bucketName
 }
 
 // Returns the gs:// URL that files in the bucket can be uploaded to,

pipeline/pkg/deploys/deploys_test.go
@@ -39,48 +39,33 @@ func TestDeploys(t *testing.T) {
 func TestDeployBuckets(t *testing.T) {
 	tests := map[string]struct {
 		envVar        string
-		wantError     bool
 		deploy        DeployType
 		testingBucket string
 	}{
-		"prod":           {envVar: "prod", deploy: DeployProduction},
-		"staging":        {envVar: "staging", deploy: DeployStaging},
-		"testing":        {envVar: "testing", deploy: DeployTesting, wantError: true},
-		"testing_bucket": {envVar: "testing", deploy: DeployTesting, testingBucket: "test-bucket-name"},
-		"blank":          {envVar: "", deploy: DeployTesting, wantError: true},
-		"blank_bucket":   {envVar: "", deploy: DeployTesting, testingBucket: "test-bucket-name"},
+		"prod":    {envVar: "prod", deploy: DeployProduction},
+		"staging": {envVar: "staging", deploy: DeployStaging},
+		"testing": {envVar: "testing", deploy: DeployTesting},
+		"blank":   {envVar: "", deploy: DeployTesting},
 	}
 	t.Cleanup(func() {
 		os.Unsetenv("DEPLOY")
-		os.Unsetenv("TESTING_BUCKET")
 	})
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
 			os.Setenv("DEPLOY", tc.envVar)
-			os.Setenv("TESTING_BUCKET", tc.testingBucket)
 			bucket, err := GetUploadURL(LegacyVersion)
-			if tc.wantError {
-				require.Error(t, err)
-				require.Equal(t, bucket, "")
-			} else {
-				require.NoError(t, err)
-				if !strings.HasPrefix(bucket, "gs://") {
-					t.Errorf("Upload URL does not start with gs://")
-				}
+			require.NoError(t, err)
+			if !strings.HasPrefix(bucket, "gs://") {
+				t.Errorf("Upload URL does not start with gs://")
 			}
 
 			url, err := GetDownloadURL(LegacyVersion)
-			if tc.wantError {
-				require.Error(t, err)
-				require.Equal(t, url, "")
-			} else {
-				require.NoError(t, err)
-				if !strings.HasPrefix(url, "https://") {
-					t.Errorf("Download URL does not start with https://")
-				}
-				if strings.HasSuffix(url, "/") {
-					t.Errorf("Download URL ends with /")
-				}
+			require.NoError(t, err)
+			if !strings.HasPrefix(url, "https://") {
+				t.Errorf("Download URL does not start with https://")
+			}
+			if strings.HasSuffix(url, "/") {
+				t.Errorf("Download URL ends with /")
 			}
 		})
 	}

pipeline/pkg/generator/generator.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 	"sync"
 	"time"
 
@@ -12,40 +11,17 @@ import (
 	"github.com/CAVaccineInventory/airtable-export/pipeline/pkg/deploys"
 	"github.com/CAVaccineInventory/airtable-export/pipeline/pkg/endpoints"
 	"github.com/CAVaccineInventory/airtable-export/pipeline/pkg/endpoints/metadata"
-	"github.com/CAVaccineInventory/airtable-export/pipeline/pkg/storage"
 	"github.com/honeycombio/beeline-go"
 	"go.opencensus.io/stats"
 	"go.opencensus.io/tag"
 )
 
-// A function which can be used to output the transformed data; see pkg/storage/
-type StorageWriter func(ctx context.Context, destinationFile string, transformedData metadata.JSONData) error
-
 type PublishManager struct {
-	store StorageWriter
 }
 
 // Publishes by uploading to GCS
 func NewPublishManager() *PublishManager {
-	return &PublishManager{
-		store: storage.UploadToGCS,
-	}
-}
-
-// Uses storage.StoreLocal to not need upload credentials
-func NewLocalPublishManager() *PublishManager {
-	os.Setenv("TESTING_BUCKET", "local")
-	return &PublishManager{
-		store: storage.StoreLocal,
-	}
-}
-
-// Uses storage.DebugToStderr to for quick debugging
-func NewNoopPublishManager() *PublishManager {
-	os.Setenv("TESTING_BUCKET", "noop")
-	return &PublishManager{
-		store: storage.DebugToSTDERR,
-	}
+	return &PublishManager{}
 }
 
 // In parallel, calls PublishEndpoint on each; if any has an error,
@@ -138,6 +114,11 @@ func (pm *PublishManager) publishEndpointActual(ctx context.Context, tables *air
 		return fmt.Errorf("failed to get destination bucket: %w", err)
 	}
 	destinationFile := bucket + "/" + ep.Resource + ".json"
+
 	log.Printf("[%v] Publishing to %s...\n", &ep, destinationFile)
-	return pm.store(ctx, destinationFile, sanitizedData)
+	store, err := deploys.GetStorage()
+	if err != nil {
+		return fmt.Errorf("failed to get storage function: %w", err)
+	}
+	return store(ctx, destinationFile, sanitizedData)
 }

pipeline/pkg/storage/local.go
@@ -20,13 +20,6 @@ func StoreLocal(ctx context.Context, destinationFile string, transformedData met
 	defer span.Send()
 	beeline.AddField(ctx, "destinationFile", destinationFile)
 
-	writeToURL, err := url.Parse(destinationFile)
-	if err != nil {
-		err = fmt.Errorf("Invalid destination URL %s: %w", destinationFile, err)
-		beeline.AddField(ctx, "error", err)
-		return err
-	}
-
 	serializedData, err := Serialize(transformedData)
 	if err != nil {
 		err = fmt.Errorf("failed to serialize json: %w", err)
@@ -34,9 +27,17 @@ func StoreLocal(ctx context.Context, destinationFile string, transformedData met
 		return err
 	}
 
-	// writeToURL.Path still has a leading `/` on it; we write out
-	// under `local`, which in Docker is mounted out to the host OS.
-	localFilePath := filepath.Join("local", writeToURL.Path)
+	writeToURL, err := url.Parse(destinationFile)
+	if err != nil {
+		err = fmt.Errorf("Invalid destination URL %s: %w", destinationFile, err)
+		beeline.AddField(ctx, "error", err)
+		return err
+	}
+
+	// Strip of the gs://; the "host" is the bucket name, which in
+	// treat as a directory.  In most cases this is "local", which,
+	// in Docker, is mounted out to the host OS.
+	localFilePath := filepath.Join(writeToURL.Host, writeToURL.Path)
 	err = os.MkdirAll(path.Dir(localFilePath), 0755)
 	if err != nil {
 		err = fmt.Errorf("failed to make directories %s: %w", path.Dir(localFilePath), err)

scripts/freshcf.sh
@@ -14,7 +14,6 @@ echo "Running image..."
 exec docker run \
 	-e "AIRTABLE_KEY=$AIRTABLE_KEY" \
 	-e "HONEYCOMB_KEY=$HONEYCOMB_KEY" \
-	-e "TESTING_BUCKET=$TESTING_BUCKET" \
 	${GOOGLE_AUTH_BIND:+'-v' "$GOOGLE_AUTH_BIND"} \
 	--rm \
 	-p 8080:8080 \

commit 4d348e44d39416cfb6f644cd7235d6906111a708

pipeline: Enable metrics by default in the server.
c7850100e2379121bc31907b93d0c38faaee3d6f defaulted metrics to on in
the server, but 7f1752dd8b463e26db561f59014b525a131fcef4 mistakenly
flipped it to false because of a copy/paste error.

pipeline/cmd/server/main.go
@@ -32,7 +32,7 @@ type Publisher struct {
 func main() {
 	noopFlag := flag.Bool("noop", false, "Only print output, don't upload")
 	bucketFlag := flag.String("bucket", "", "Upload into a specific bucket")
-	metricsFlag := flag.Bool("metrics", false, "Enable metrics reporting")
+	metricsFlag := flag.Bool("metrics", true, "Enable metrics reporting")
 	flag.Parse()
 
 	if *noopFlag && *bucketFlag != "" {